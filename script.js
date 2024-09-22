5bead7dcabaeaccc99cf0fdd30fedfaf0ed61e82
Nabin Poudel <nabinpoudel2061@gmail.com>
2024-09-22 19:37:49 +0545

    First Commit of JS

---
+++ script.js
@@ -0,0 +1,8 @@
+document.write("Hello World!");
+document.write("<h1>Hello World!</h1>");
+console.log("BCa Fourth Sem");
+
+document.getElementById("heading").innerHTML = "We are reading HTML";
+alert("This is BCA Class");
+
+
